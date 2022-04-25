d9402d272337697d57694ba9e1690413de2d7a65
YIAHN404-FASTFIVE <54733637+ahnanne@users.noreply.github.com>
2022-04-25 13:03:20 +0900

    [A] 공통 인풋 컴포넌트

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,55 @@
+import styled from '@emotion/styled';
+
+const Wrap = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+const Label = styled.label`
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+const CustomInput = styled.input`
+  width: 300px;
+  height: 40px;
+  border: 1px solid #bebebe;
+  border-radius: 8px;
+  margin-top: 8px;
+  padding: 0 10px;
+`;
+
+const SubText = styled.p`
+  color: #999;
+  font-size: 12px;
+  font-style: italic;
+  margin-top: 4px;
+`;
+
+type Props = {
+  id: string;
+  label: string;
+  value: string;
+  setValue: (v: string) => void;
+  subText?: string;
+  style?: React.CSSProperties;
+};
+
+const Input = ({ id, label, value, setValue, subText, style }: Props) => (
+  <Wrap style={style}>
+    <Label htmlFor={id}>{label}</Label>
+
+    <CustomInput
+      type="text"
+      id={id}
+      name={id}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+    <SubText>{subText}</SubText>
+  </Wrap>
+);
+
+export default Input;
