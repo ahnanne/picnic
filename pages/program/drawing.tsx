5e23d594cf9acaf97007b96b3bf09be8e1391c2d
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-04-27 03:05:25 +0900

    [A] 미술시간 페이지(작업 중)

--- pages/program/drawing.tsx
+++ pages/program/drawing.tsx
@@ -1,7 +1,8 @@
 import Layout from '@/components/layout/Layout';
 
+/** https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=2010smart&logNo=221090831764 */
 const Drawing = () => (
-  <Layout title='프로그램2'>
+  <Layout title='미술시간'>
     ...
   </Layout>
 );
