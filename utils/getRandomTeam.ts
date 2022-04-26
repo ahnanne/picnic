a03ac6e03c06b4a7d401323c7bbbac2852052300
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-04-26 22:57:31 +0900

    [F] 팀짜기 로직 수정

--- utils/getRandomTeam.ts
+++ utils/getRandomTeam.ts
@@ -5,6 +5,14 @@ const generateRandomKey = () => {
 };
 
 /* -------------------------------------------------------------------------- */
+export type TeamType = {
+  teamName: string;
+  members: {
+    name: string;
+    key: number;
+  }[];
+};
+
 type Params = {
   members: string;
   /** 한 팀당 인원 */
@@ -16,16 +24,16 @@ const getRandomTeam = ({ members, teamMemberNumber }: Params) => {
   const memberList = splitter(members, ',');
 
   // 각 멤버에게 random key 부여
-  const membersWithKey = memberList.map((member) => ({ name: member, key: generateRandomKey() }));
+  const membersWithKey = memberList.map((member) => ({ name: member.trim(), key: generateRandomKey() }));
 
   // key 값을 기준으로 정렬
   membersWithKey.sort((a, b) => a.key - b.key);
 
   const totalTeam = Math.ceil(memberList.length / teamMemberNumber);
 
-  const randomTeam = new Array(totalTeam).fill(0).map((_, idx) => ({
+  const randomTeam: Array<TeamType> = new Array(totalTeam).fill(0).map((_, idx) => ({
     teamName: `${idx + 1}팀`,
-    members: membersWithKey.slice(idx * totalTeam, ((idx + 1) * totalTeam)),
+    members: membersWithKey.slice(idx * teamMemberNumber, ((idx + 1) * teamMemberNumber)),
   }));
 
   return randomTeam;
