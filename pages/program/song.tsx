de31303aaf4a1d2bd3648f0b54ffb84f1a548a8b
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-04-27 03:04:54 +0900

    [A] 음악시간 페이지

--- pages/program/song.tsx
+++ pages/program/song.tsx
@@ -1,14 +1,30 @@
 import styled from '@emotion/styled';
+
 import Layout from '@/components/layout/Layout';
+import Program from '@/components/common/Program';
+import jyp from '@/assets/jyp.jpg';
 
 const Container = styled.div`
   padding: 40px;
 `;
 
+const Text = styled.p`
+  font-size: 24px;
+`;
+
+
 const Song = () => (
-  <Layout title='프로그램1'>
+  <Layout title='음악시간'>
     <Container>
-      test
+      <Program
+        backgroundImage={jyp}
+        mainTitle='무슨 노래일까요?'
+        subTitle='노래 1초 듣고 제목 알아맞히기'
+      >
+        <Text>
+          주제: 2010년대 히트곡
+        </Text>
+      </Program>
     </Container>
   </Layout>
 );
